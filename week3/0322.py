0f0d10481e6fa6bfa3874ee36673ef8113c2be54
배진호 <bse06119@naver.com>
2023-03-22 16:54:45 +0900

    add week3

---
+++ week3/0322.py
@@ -0,0 +1,21 @@
+# 2023-03-22
+'''
+wwwwww
+
+여러줄짜리 주석
+'''
+print("파이썬 수업 "*3)
+print('hello' + 'python')
+print(2)
+print(58.5)
+print(type('안녕'))
+print(type(123))
+print("hello ","hi","213"," ",1111)
+
+print("3+8 = ",3+8)
+print(eval('3+8'))
+
+var = '3+8'
+print(eval(var)) # eval 활용
+
+
---
+++ week3/test02.py
@@ -0,0 +1,12 @@
+money = 78000
+
+print('5만원 ',money//50000,'개')
+money = money % 50000
+
+print('1만원 ',money//10000,'개')
+money = money % 10000
+
+print('5천원 ',money//5000,'개')
+money = money % 5000
+
+print('잔돈 ',money,'원')
